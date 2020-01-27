6eb254606a2b2dc94bf910d974642d8e01cebac9
vihainen <gabriel.simoes.esilva@gmail.com>
2020-01-27 03:22:39 -0300

    Updates

---
+++ precache-manifest.2efc4810d1634e87720dc309d7863822.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91f40f8b0493d24c36ad7f1c9597989a",
+    "url": "/swapi_test/index.html"
+  },
+  {
+    "revision": "5509fb1b4500f04385e1",
+    "url": "/swapi_test/static/css/main.b7d0780e.chunk.css"
+  },
+  {
+    "revision": "07db1778bf970bad341a",
+    "url": "/swapi_test/static/js/2.5f2dbad8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/swapi_test/static/js/2.5f2dbad8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5509fb1b4500f04385e1",
+    "url": "/swapi_test/static/js/main.522f6e80.chunk.js"
+  },
+  {
+    "revision": "a0cbe9b9c5bc8be26d17",
+    "url": "/swapi_test/static/js/runtime-main.691b1c56.js"
+  }
+]);
